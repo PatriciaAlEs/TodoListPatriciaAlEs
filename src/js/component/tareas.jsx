957d7010e54b648ec6bc9d331689d036d46d42e0
PatriciaAlEs <168550797+PatriciaAlEs@users.noreply.github.com>
2024-08-15 15:47:22 +0000

    se guardan en la consola las tareas

--- src/js/component/tareas.jsx
+++ src/js/component/tareas.jsx
@@ -63,7 +63,19 @@ const ListaDeTareas = () => {
     }
 
 
-    //try y catch es manejo de errores? 
+    const obtenerArrayApi = async () => {
+        try {
+            //condicion para no entrar en bucle
+            const response = await fetch("https://playground.4geeks.com/todo/users/HarryPotter")
+            // fetch(consulta) recibe como argumento la URL donde hacemos la peticion para la obtencion de datos/asincrono! promesa pendiente hasta el .then
+            const data = await response.json();
+            //convertimos la respuesta en un json (o texto o lo que sea)
+            console.log(data)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 
     //esto hará que carguen las tareas al inicio porque se ejecuta obtenerTarea
@@ -71,11 +83,12 @@ const ListaDeTareas = () => {
 
     useEffect(() => {
         createUser()
+        obtenerArrayApi()
     }, [])
     // funcion para obtener la lista de tareas al montarse el componente 
 
 
-    const crearTarea = async (tarea) => {
+    const crearTarea = async () => {
         try {
             const response = await fetch("https://playground.4geeks.com/todo/todos/HarryPotter", {
                 method: "POST",
@@ -87,83 +100,84 @@ const ListaDeTareas = () => {
                     is_done: false
                 })
             });
-    
+
             const data = await response.json();
             console.log(data);
-    
+
         } catch (error) {
             console.log(error);
         }
-    
+
         setLista([...lista, tarea]); // Agrega la nueva tarea a la lista actual
         setTarea(""); // Limpia el input de tarea
     }
-    
-    
-
-//para eliminar una tarea de la lista 
-const eliminarTarea = (indexItem) => {
-    setLista((prevState) => prevState.filter((_, index) => index !== indexItem))
-}
 
 
-// funcion para manejar la Tarea con la API?
-const manejarTarea = (event) => {
-    setTarea(event.target.value)
-}
 
-
-const teclaAgregaNuevaTarea = (event) => {
-    if (event.key === "Enter") {
-        crearTarea(tarea);
+    //para eliminar una tarea de la lista 
+    const eliminarTarea = (indexItem) => {
+        setLista((prevState) => prevState.filter((_, index) => index !== indexItem))
     }
-}
-// funcion para al presionar que imprime un nuevo elemento en la lista
+
+
+    // funcion para manejar la Tarea con la API?
+    const manejarTarea = (event) => {
+        setTarea(event.target.value)
+    }
+
+
+    const teclaAgregaNuevaTarea = (event) => {
+        if (event.key === "Enter") {
+            crearTarea(tarea);
+        }
+    }
+    // funcion para al presionar que imprime un nuevo elemento en la lista
 
 
 
-const tareaVacia = lista == "" ? "No hay tareas, añade tu tarea" : " Introduzca otra tarea";
+
+
+    const tareaVacia = lista == "" ? "No hay tareas, añade tu tarea" : " Introduzca otra tarea";
+
+
+    if (lista)
+        return (
+            <div>
+                <input
+                    type="text"
+                    placeholder={tareaVacia}
+                    value={tarea}
+                    // el valor es el nombre de la primera variable de estado y su valor useState("") por eso empieza vacío el input
+                    onChange={manejarTarea}
+                    // llamamos a la funcion manejarTarea
+                    onKeyDown={teclaAgregaNuevaTarea}
+                // llamamos a la funcion teclaAgregaNuevaTarea
+                >
+                </input>
+
+                <ul>
+
+                    {lista.map((cosas, index) => (
+                        <li key={index} className="basurahover">{cosas}
+                            {/* onClick siempre tiene un callback que seria un evento a no ser que le pasemos otro parametro dentro de la funcion (en este caso la funcion es eliminarTarea, y el parametro, (index)) */}
+                            <div className="misbotones">
+                                <BotonCheck />
+                                <button className="btn" onClick={() => eliminarTarea(index)}>
+                                    {/* //el icono de la basura va dentro del li */}
+
+                                    <i className="fas fa-trash-alt" />
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+
+                </ul>
+                <div className="cuantasTareas"> Total de tareas por hacer: {lista.length}</div>
 
 
 
-if (lista)
-    return (
-        <div>
-            <input
-                type="text"
-                placeholder={tareaVacia}
-                value={tarea}
-                // el valor es el nombre de la primera variable de estado y su valor useState("") por eso empieza vacío el input
-                onChange={manejarTarea}
-                // llamamos a la funcion manejarTarea
-                onKeyDown={teclaAgregaNuevaTarea}
-            // llamamos a la funcion teclaAgregaNuevaTarea
-            >
-            </input>
-
-            <ul>
-
-                {lista.map((cosas, index) => (
-                    <li key={index} className="basurahover">{cosas}
-                        {/* onClick siempre tiene un callback que seria un evento a no ser que le pasemos otro parametro dentro de la funcion (en este caso la funcion es eliminarTarea, y el parametro, (index)) */}
-                        <div className="misbotones">
-                            <BotonCheck />
-                            <button className="btn" onClick={() => eliminarTarea(index)}>
-                                {/* //el icono de la basura va dentro del li */}
-
-                                <i className="fas fa-trash-alt" />
-                            </button>
-                        </div>
-                    </li>
-                ))}
-
-            </ul>
-            <div className="cuantasTareas"> Total de tareas por hacer: {lista.length}</div>
-
-
-
-        </div>
-    )
+            </div>
+        )
 }
 
 
